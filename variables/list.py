b8888850d5b8ef109e1f0882b22dd2ca2488d6a3
smalvee <sm.raisuddinalvee@gmail.com>
2022-08-26 12:39:44 +0600

    listing matarials

---
+++ variables/list.py
@@ -0,0 +1,58 @@
+# student1 = "devid"
+# email = "mail@gmail.com"
+# phone = "123456789"
+# address = "dhaka"
+#
+# student2 = "devid"
+# email = "mail@gmail.com"
+# phone = "123456789"
+# address = "dhaka"
+
+# student1 = ['devid', 'devid@gmail.com', '123456678', 'Dhaka']
+# student2 = ['Smith', 'smith@gmail.com', '09876543', 'Chittagong']
+#
+# print(student1[1])
+# print(len(student1))
+#
+# student1_name = student1[0]
+# student2_name = student2[0]
+# print(student1_name)
+# print(student2_name)
+#
+#
+# guest = ['gest1', 'gest2', 'gest3', 'gest4', 'gest5', 'gest6', 'gest7', 'gest8', 'gest9', 'gest10']
+#
+# print("Guest list for lunche")
+#
+# lunch = guest[0:4]
+# print(lunch)
+#
+#
+# print("Guest list for Dinner")
+#
+# lunch = guest[4:9]
+# print(lunch)
+
+numbers = [1, 3, 4, 2, 11, 76, 0]
+numbers.sort()
+print(numbers)
+
+numbers.insert(3, 10)
+print(numbers)
+
+numbers.remove(11)
+print(numbers)
+
+numbers.pop(1)
+print(numbers)
+
+numbers.reverse()
+print(numbers)
+
+numbers.append(99)
+print(numbers)
+
+numbers.clear()
+print(numbers)
+
+
